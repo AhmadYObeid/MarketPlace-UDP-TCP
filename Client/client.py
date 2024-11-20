a2a7e0d5cb9d032207096bc02afd6d069894fd21
AhmadO <ahmadyaobeid@gmail.com>
2024-11-19 22:01:40 -0500

    save progress

--- Client/client.py
+++ Client/client.py
@@ -1,111 +1,145 @@
-import socket
+import socket 
 import sys
 import json
-import threading
-import random
+import time #library used to simulate simutanous client reqeuest sent to 
+            #the server for multi-threading test purposes.
+            #It can also be used to set timers for client offers.
 
-# Socket parameters
-client_host = "0.0.0.0"
-client_port = 2000
-client_TCP_port = 2100
+#defining the socket parameters (IP + PORT)
+client_host = "0.0.0.0" #listening on all available netwrok interfaces
+client_port = 2000 #arbitrary port number for the client socket
+#defining the TCP port number for testing purposes
+client_TCP_port = 2100 #arbitrary number for now
+
+#Loads the current value of the request_number from the client_config.json file
+def load_request_number():
+  try:
+    with open('client_config.json', 'r') as json_file:
+      data = json.load(json_file)
+      return data
+  except FileNotFoundError:
+    return 1 #returns 1 if file is not exisiting yet, meaning we are starting fresh
+  except json.JSONDecodeError:
+    return 1 #returns 1 if the file exists but is empty 
+
+#Updates the request_number in the client_config.json file
+#Called everytime a request is sent to the server
+def update_request_number(request_number):
+  with open("client_config.json", "w") as json_file:
+    json.dump(request_number, json_file, indent=4)
+
+#Initializing the request_number by loading the current request_number from the config file
+request_number = load_request_number()
+
+#Creating the UDP clinet socket
+try:
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+  print(f"Socket creation failed!")
+  sys.exit()
+
+#Binding the socket to its parameters
+s.bind((client_host, client_port))
+
+#Socket parameters of the server socket
 server_host = "localhost"
 server_port = 5000
 
-# Load and update request number
-def load_request_number():
-    try:
-        with open('client_config.json', 'r') as json_file:
-            data = json.load(json_file)
-            return data
-    except (FileNotFoundError, json.JSONDecodeError):
-        return 1
-
-def update_request_number(request_number):
-    with open("client_config.json", "w") as json_file:
-        json.dump(request_number, json_file, indent=4)
-
-request_number = load_request_number()
-
-# UDP client socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except socket.error:
-    print("Socket creation failed!")
-    sys.exit()
-
-s.bind((client_host, client_port))
-is_registered = False  # To track registration status
-
-# User actions
+#Variable to track the registration status of the client
+#Used to allow the users to use some functions only if they are registered
+is_registered = False
+ 
+#Swtich statement to handle different user requests
 def user_request(user_input):
     match user_input:
-        case 1:
-            return user_registration(request_number)
-        case 2:
-            return user_deregistration(request_number)
-        case 3:
-            return item_search(request_number)
-        case _:
-            print("Invalid option! Please try again.")
+      case 1:
+        return user_registration(request_number) 
+      case 2:
+        return user_deregistration(request_number)
+      case 3:
+        return looking_for(request_number)
+      case _:
+        print(f"Invalid option! Please try again.")
 
-# Registration
+#Prepares the registration request message to be sent to the server
 def user_registration(request_number):
-    global is_registered
-    print("Enter registration details:")
-    name = input("Name: ")
-    ip = socket.gethostbyname(socket.gethostname())
-    udp = client_port
-    tcp = client_TCP_port
-    msg = f"REGISTER, {str(request_number)}, {name}, {ip}, {udp}, {tcp}"
-    is_registered = True
-    return msg
+  print(f"Enter the following information to register yourself within the shopping system:\n")
+  name = input(f"Name: ")
+  ip = socket.gethostbyname(socket.gethostname())
+  udp = client_port
+  tcp = client_TCP_port
+  msg = f"REGISTER, {str(request_number)}, {name}, {ip}, {udp}, {tcp}"
+  global is_registered 
+  is_registered = True #TODO: only a temporary solution for now, eventually we should wait for the server's response to set this value to true.
+  return msg
 
-# De-registration
+#Prepares the de-registration request message to be sent to the server
 def user_deregistration(request_number):
-    global is_registered
-    name = input("Enter the name of the user to de-register: ")
-    ip = socket.gethostbyname(socket.gethostname())
-    udp = client_port
-    msg = f"DE-REGISTER, {name}, {ip}, {udp}"
-    is_registered = False
-    return msg
+  name = input(f"Enter the name of the user to be de-registered: ")
+  msg = f"DE-REGISTER, {name}"
+  global is_registered 
+  is_registered = False
+  return msg
 
-# Search for items
-def item_search(request_number):
-    if not is_registered:
-        print("You must register before searching for items!")
-        return None
-    print("Enter details for item search:")
-    name = input("Your Name: ")
+print(f"Welcome to the Peer-to-Peer Shopping System!\n")
+
+#Sends a looking_for request to the server to indicate a search for an item
+def looking_for(reqeuest_number):
+  if (is_registered == True):
+    name = input("Enter your name: ") #TODO: potentially replace this with a variable that holds the name of
+    #the user once they register. However, that might be a problem if the client creats multiple
+    #users on the same device.
+    print("What item are you looking for:")
     item_name = input("Item Name: ")
     item_description = input("Item Description: ")
     max_price = input("Max Price: ")
     msg = f"LOOKING_FOR, {str(request_number)}, {name}, {item_name}, {item_description}, {max_price}"
     return msg
+  else:
+    return None
+    
 
-print("Welcome to the Peer-to-Peer Shopping System!")
+# #just a method to test multi request transmission (uncomment to use)
+# def test_multiThreading():
+#   name = 'Larry'
+#   ip = socket.gethostbyname(socket.gethostname())
+#   udp = client_port
+#   tcp = client_TCP_port
+#   msg = f"REGISTER, {str(request_number)}, {name}, {ip}, {udp}, {tcp}"
+#   return msg
 
-# Main loop
+#Sending a message to the server
 while True:
-    user_input = int(input("""Choose an option:
+#   #Prompting the user to choose a feature offered by the shopping system
+  user_input = int(input(f"""Please choose one of the options below:\n
   [1] - Register
   [2] - De-Register
-  [3] - Search for an Item
--> """))
-
-    request_msg = user_request(user_input)
-    if request_msg is None:
-        continue
-
+  [3] - Look for an Item
+                
+->"""))
+  
+#   #Processing the user's input and saving it 
+  request_msg = user_request(user_input)
+  
+  # num_requests = 2 #Arbitrary number of requests to send multiple requests at once
+  if (request_msg != None):
     try:
-        s.sendto(request_msg.encode('utf-8'), (server_host, server_port))
-        request_number += 1
-        update_request_number(request_number)
+        s.sendto(request_msg.encode('utf-8'), (server_host,server_port)) #the string message is encoded into bytes before being sent to the server using the utf-8 encoding standard
+        request_number += 1 #incrementing the request number for the next requests to be sent
+        update_request_number(request_number) #updating the client_config.json file with the newest request number
 
-        # Receive server reply
+        #storing the reply message of the server upon receiving the client request
         d = s.recvfrom(1024)
-        reply = d[0].decode('utf-8')
+        reply = d[0].decode('utf-8') #Decode the message provided by the server into a string using the utf-8 decoding standard
         addr = d[1]
+
+        #displaying the server's reply message
         print(f"\nServer reply [{addr[0]}, {addr[1]}]: {reply}\n")
-    except socket.error:
-        print("Error occurred!")
+
+        #small delay between requests to simulate a new client request
+        #time.sleep(0.1)
+
+    except socket.error as msg:
+      print("Error Occured!")
+  else:
+    print("User not registered! Please register first.")
--- Server/server.py
+++ Server/server.py
@@ -1,111 +1,155 @@
 import socket
-import threading
+import threading #Threading library to make the server multi-threaded
 import sys
 import json
-import random
 
-# Server parameters
-HOST = '0.0.0.0'
-PORT = 5000
+#Defining the socket parameters
+HOST = '0.0.0.0' #listening on all available netwrok interfaces
+PORT = 5000 #arbitrary port number chosen for the server socket
 
-# UDP socket creation and binding
+#Creating the UDP socket
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print("Socket created!\n")
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  print("Socket Created!\n")
 except OSError as e:
-    print(f"Socket creation failed: {e}")
-    sys.exit()
-
+  print (f"Failed to create the socket.\nError Code: {e.errno}\nMessage: {e.strerror}\n")
+  sys.exit()
+  
+#Binding the socket to the host and port number
 try:
-    s.bind((HOST, PORT))
+  s.bind((HOST,PORT))
 except OSError as e:
-    print(f"Binding failed: {e}")
-    sys.exit()
+  print (f"Binding failed!\nError Code: {e.errno}\nMessage: {e.strerror}\n")
+  sys.exit()
 
-print("Socket successfully bound!\n")
+print ("Socket was successfuly bound!\n")
 
+#Setting a timeout for the socket
+#If no data is received within 2 seconds, the timeout exception is raised
+#This allows the program to escape from the infinite loop of recvfrom()
+#Used to allow KeyboardInterrupt to trigger
 s.settimeout(5)
 
-# Users and items
-try:
-    with open('users.json', 'r') as json_file:
-        users = json.load(json_file)
-except FileNotFoundError:
-    users = {}
+#Implements a switch statement to handle the different requests coming from the client
+def handle_request(user_request, data, addr):
+  match user_request:
+    case "REGISTER":
+      return handle_registration(data, addr)
+    case "DE-REGISTER":
+      return handle_deregistration(data, addr)
+    case "LOOKING_FOR":
+      return handle_looking_for(data, addr)
+    case _:
+      print("Invalid Request!")
 
-items = {}  # Items available from users
+#Creating a dictionary to store the users information within the server (the dictionary is restored using the json file everytime the server runs again)
+try:
+  with open('users.json', 'r') as json_file:
+    users = json.load(json_file) #loading the json file users' info into the dictionary of users
+  
+except FileNotFoundError:
+  users = {} #JSON file does not exist, meaning no users have registred yet, therefore we start fresh
+
+#Creating a mutex lock to avoid race conditions for the users.json file
 lock = threading.Lock()
 
-# Request handler
-def handle_request(request_type, data, addr):
-    match request_type:
-        case "REGISTER":
-            return handle_registration(data, addr)
-        case "DE-REGISTER":
-            return handle_deregistration(data, addr)
-        case "LOOKING_FOR":
-            return handle_search_request(data, addr)
-        case "OFFER":
-            return handle_offer(data, addr)
-        case _:
-            print("Invalid request!")
-
-# Registration handler
+#Handles the register request
 def handle_registration(data, addr):
-    _, request_number, name, ip, udp, tcp = data.split(", ")
-    lock.acquire()
-    try:
-        if name not in users:
-            users[name] = {"ip": ip, "udp": udp, "tcp": tcp}
-            with open('users.json', 'w') as json_file:
-                json.dump(users, json_file, indent=4)
-            reply_msg = f"REGISTERED, {request_number}"
-        else:
-            reply_msg = f"REGISTER-DENIED, {request_number}, User already exists!"
-    finally:
-        lock.release()
+  request_type, request_number, name, ip, udp, tcp = data.split(", ") #splitting the data into the different fields provided
+  #TODO: Add the condition where the server cannot add any more clients
+  #Acquiring the lock before modifying the 'users' dictionary
+  lock.acquire()
+  #Encapsulating the lock release within a try-finally to ensure its release even if an error occurs
+  try:
+    if name not in users:
+    #Adding the user into the users dictionary
+      users[name] = {
+        "ip": ip,
+        "udp": udp,
+        "tcp": tcp
+      }
+      #Save the user info in the users.json file
+      with open('users.json', 'w') as json_file:
+          json.dump(users, json_file, indent=4)
 
-    s.sendto(reply_msg.encode('utf-8'), addr)
+      reply_msg = f"REGISTERED, {request_number}"
+    else:
+      reply_msg = f"REGISTER-DENIED, {request_number}, The user already exists in the system!"
+  finally:
+    lock.release() #Releasing the lock after successful modification
 
-# De-registration handler
+  #Sending the handled request message back to the client
+  s.sendto(reply_msg.encode('utf-8'), addr)
+  print(f"Message received from [{addr[0]}, {addr[1]}]: {data}")
+
+#Handles the de-registration request 
 def handle_deregistration(data, addr):
-    _, name, client_ip, client_udp = data.split(", ")
-    lock.acquire()
-    try:
-        if name in users and users[name]["ip"] == client_ip and users[name]["udp"] == client_udp:
-            del users[name]
-            with open('users.json', 'w') as json_file:
-                json.dump(users, json_file, indent=4)
-            reply_msg = f"User [{name}] removed!"
-        else:
-            reply_msg = f"De-registration failed!"
-    finally:
-        lock.release()
+  request_type, name = data.split(", ") 
 
-    s.sendto(reply_msg.encode('utf-8'), addr)
+  #Acquiring the lock before modifying the 'users' dictionary
+  lock.acquire()
+  #Encapsulating the lock release within a try-finally to ensure its release even if an error occurs
+  try:
+    if name in users:
+      del users[name] #Deleting the user from the dictionary of users
+      # Updating the users.json file with the changes user changes
+      with open('users.json', 'w') as json_file:
+          json.dump(users, json_file, indent=4)
+      reply_msg = f"User [{name}] was successfully removed from the server!\n"
+    else:
+      reply_msg = f"User [{name}] does not exist in the server!"
+  finally:
+    lock.release() #Releasing the lock after successful modification
 
-# Search handler
-def handle_search_request(data, addr):
-    _, request_number, buyer_name, item_name, item_description, max_price = data.split(", ")
-    search_msg = f"SEARCH, {request_number}, {item_name}, {item_description}"
-    for user, details in users.items():
-        if user != buyer_name:
-            s.sendto(search_msg.encode('utf-8'), (details["ip"], int(details["udp"])))
+  #Sending the handled request message back to the client
+  s.sendto(reply_msg.encode('utf-8'), addr)
+  print(f"Message received from [{addr[0]}, {addr[1]}]: {data}")
 
-# Offer handler
-def handle_offer(data, addr):
-    _, request_number, name, item_name, price = data.split(", ")
-    print(f"Offer received: {name} offers {item_name} for {price}")
+#Handles the looking_for request
+def handle_looking_for(data, addr):
+  request_type, reqeuest_number, name, item_name, item_description, max_price = data.split(", ")
+  #Preparing the reply message to be sent to all client of the system
+  search_msg = f"SEARCH, RQ#, {item_name}, {item_description}"
 
-# Main loop
-while True:
-    try:
-        d = s.recvfrom(1024)
-        data = d[0].decode('utf-8')
-        addr = d[1]
-        request_type = data.split(",")[0]
-        threading.Thread(target=handle_request, args=(request_type, data, addr)).start()
-    except socket.timeout:
+  print(f"Message received from [{addr[0]}, {addr[1]}]: {data}")
+
+  #Acquiring the lock before reading from the users dictionary
+  lock.acquire()
+
+  try:
+    for user in users:
+      if user != name:
+        #Sending the search message to all clients except the buyer
+        s.sendto(search_msg.encode('utf-8'), (users[user]["ip"], int(users[user]["udp"])))
+      else:
         pass
+  finally:
+    lock.release()
 
+  #TODO: add the reply_msg to the client here.
+       
+
+#Listening for client requests indefinitely
+while True:
+  try:
+    
+    d = s.recvfrom(1024) #receiving the data from the client with a buffer size of 1024 bytes
+    data = d[0].decode('utf-8') #storing the data after decoding it into string type
+    addr = d[1] #storing the socket parameters of the client (IP + PORT)
+
+    #break the loop if there is no data sent from the client
+    if not data:
+      break
+    #Multi-threading
+    #Once the listener detects a client request, it creates a new thread and passes the handle_request method as target to handle the request
+    else:
+      user_request = data.split(',')[0] #splitting the request message sent by the client to only have the type of request (e.i. REGISTER, DE-REGISTER, etc...
+      client_thread = threading.Thread(target=handle_request, args=(user_request,data,addr)) #Creating a new thread to handle the coming client request
+      client_thread.start() #starting the thread
+      client_thread.join() #waiting for the thread to be complete before the next one can resume
+  
+  except socket.timeout:
+    pass #Continues to the next iteration without performing any error handling action
+
+#closing the UDP socket
 s.close()
